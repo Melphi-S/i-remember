49494a41879ce6b8a3771ddbd8c940abcd06e1c2
Melphi-S <imaginarylight@yandex.ru>
2024-02-15 19:55:46 +0300

    make config

---
+++ backend/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { TypeOrmConfigService } from './config/database-config.factory';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/config/configuration.ts
@@ -0,0 +1,20 @@
+export default () => ({
+    server: {
+        port: process.env.SERVER_PORT || 3000,
+        cors_origins: process.env.CORS_ORIGINS,
+    },
+    database: {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        name: process.env.POSTGRES_NAME,
+    },
+    jwt: {
+        key: process.env.JWT_KEY,
+        ttl: process.env.JWT_TTL || '7d',
+    },
+    hash: {
+        salt: Number(process.env.SALT) || 10,
+    },
+});
